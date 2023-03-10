8d633d3f19e43d3c509c1171c56c32573c5ce782
Radoslav Filipov <42972410+fedarrdi@users.noreply.github.com>
2023-03-10 11:11:01 -0400

    Add files via upload

--- Firewall/firewall.py
+++ Firewall/firewall.py
@@ -30,19 +30,26 @@ def cmp_mac_address_start(curr_mac_address, starts):
             return True
     return False
 
+
 def make_dict(packet):
     packet_info={}
     try:
         time = datetime.datetime.now()
         packet_info['localtime'] = time.year*10000000000 + time.month * 100000000 + time.day * 1000000 + time.hour*10000 + time.minute*100 + time.second
-        packet_info['src_addr']  = int(packet.ip.src.replace('.',''))             # source address
         packet_info['src_port']  = int(packet[packet.transport_layer].srcport)   # source port
         packet_info['src_mac']   = int(packet.eth.src.replace(':',''),16)         # source mac
-        packet_info['dst_addr']  = int(packet.ip.dst.replace('.',''))             # destination address
         packet_info['dst_port']  = int(packet[packet.transport_layer].dstport)   # destination port
         packet_info['dst_mac']   = int(packet.eth.dst.replace(':',''),16)         # destination mac
         packet_info['size']      = len(packet)                                    # length of the packet
         packet_info['malicious'] = 0
+    
+        if 'IP' in packet:
+            packet_info['dst_addr']  = int(packet.ip.dst.replace('.',''))             # destination address
+            packet_info['src_addr']  = int(packet.ip.src.replace('.',''))             # source address
+        if 'IPv6' in packet:
+            packet_info['dst_addr']  = int(packet.ip.dst.replace(':',''), 16)             # destination address
+            packet_info['src_addr']  = int(packet.ip.src.replace(':',''), 16)             # source address
+
     except AttributeError as e:
         # ignore packets other than TCP, UDP and IPv4
         return None
@@ -113,14 +120,10 @@ print(starts)
 capture = pyshark.LiveCapture(interface='wlp2s0')
 capture.sniff(timeout=10)
 print("aaa")
-for packet in capture:
-    try:
-        if cmp_mac_address_start(packet.eth.src,starts) == False:
-            pass
-    except AttributeError as e:
-        pass
 
-    print(packet)
+for packet in capture:
+    if 'ETH Layer' in str(packet.layers) and ('IP' in packet or 'IPv6' in packet) and cmp_mac_address_start(packet.eth.src,starts):
+        print(packet)
     
     #row = make_dict(packet)
     #source, destination = get_source_and_destination(row)
