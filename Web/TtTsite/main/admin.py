f06288f246ade4eba27d99f886c80d16437daf36
Yoan Laptop <yoanivanov9@gmail.com>
2023-03-11 00:36:06 +0200

    added superuser, only he can view

--- Web/TtTsite/main/admin.py
+++ Web/TtTsite/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import IotDevice, Whitelist, Blacklist
 # Register your models here.
+
+admin.site.register(IotDevice)
+admin.site.register(Whitelist)
+admin.site.register(Blacklist)
