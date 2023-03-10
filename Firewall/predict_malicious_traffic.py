513b1331f0b17f12956740b9c6705e94889d1967
Jivko <jivkon1410@gmail.com>
2023-03-10 21:52:31 +0200

    ml testing

--- Firewall/predict_malicious_traffic.py
+++ Firewall/predict_malicious_traffic.py
@@ -3,23 +3,6 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import pandas as pd
 
-df = pd.read_csv("traffic.csv")
-df.head()
-
-# %matplotlib inline
-# plt.scatter(df['Mileage'], df['Sell Price($)'])
-
-X = df[['src_addr', 'src_port', 'src_mac', 'dst_addr', 'dst_port', 'dst_mac', 'size']]
-y = df['malicious']
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-
-model = LinearRegression()
-model.fit(X_train, y_train)
-
-prediction = model.predict(X_test)
-score = model.score(X_test, y_test)
-
 def get_prediction(input):
 
     df = pd.read_csv("traffic.csv")
