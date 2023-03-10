757135e57fbafd31e941b970f0a14d7121908981
Yoan Laptop <yoanivanov9@gmail.com>
2023-03-10 15:36:41 +0200

    only move button not working

--- Web/TtTsite/main/views.py
+++ Web/TtTsite/main/views.py
@@ -8,10 +8,40 @@ from django.urls import reverse
 # Create your views here.
 
 def index(response,id):
+    # device = IotDevice.objects.get(id=id)
+    # return render(response,"main/list.html",{"device":device})
     device = IotDevice.objects.get(id=id)
-    # whitelist = device.whitelist_set.all()
-    # blacklist = device.blacklist_set.all()
+ 
+    if response.method == "POST":
+        if response.POST.get("newItemWhitelist"):
+            txt = response.POST.get("new")    
+            if len(txt) > 2:
+                device.whitelist_set.create(ip=txt)
+            else:
+                print("invalid")
+        elif response.POST.get("newItemBlacklist"):
+            txt = response.POST.get("new")    
+            if len(txt) > 2:
+                device.blacklist_set.create(ip=txt)
+            else:
+                print("invalid")
+               
+        elif response.POST.get("moveToWhitelist"):
+            print("AAAAAAAAA" + response.POST.get("moveToWhitelist"))
+            # id = ...
+            # tempip = 
+            # device.blacklist_set.all()[id].delete()
+            # device.blacklist_set.create(ip=tempip)
+
+        elif response.POST.get("moveToBlacklist"):
+            pass
+            # id = ...
+            # tempip = 
+            # device.blacklist_set.all()[id].delete()# delete from whitelist
+            # device.blacklist_set.create(ip=tempip)# add to blacklist
+                              
     return render(response,"main/list.html",{"device":device})
+
 
 def home(response):
     devices = IotDevice.objects.all()
