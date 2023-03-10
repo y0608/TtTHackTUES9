951e0378ba354efcd33b6faaf4159bf73c5595f1
Yoan Laptop <yoanivanov9@gmail.com>
2023-03-10 13:13:46 +0200

    ima homepage

--- Web/TtTsite/main/views.py
+++ Web/TtTsite/main/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import IotDevice, Whitelist, Blacklist
+
+from django.shortcuts import render
+from django.urls import reverse
+
 # Create your views here.
 
 def index(response,id):
-    devices = IotDevice.objects.get(id=id)
-    whitelist = devices.whitelist_set.all()
-    blacklist = devices.blacklist_set.all()
-    return HttpResponse("<h1>%s</h1>" % blacklist[0])
+    device = IotDevice.objects.get(id=id)
+    # whitelist = device.whitelist_set.all()
+    # blacklist = device.blacklist_set.all()
+    return render(response,"main/list.html",{"device":device})
+
+def home(response):
+    devices = IotDevice.objects.all()
+    return render(response,"main/home.html",{"devices":devices})
